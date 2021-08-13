cd44ba677a7103bba31e3ddf185d09c863b39e9b
bonisiweinnocent <bonisiwe.cukatha@gmail.com>
2021-08-13 15:43:44 +0200

    testing

--- test/settings.test.js
+++ test/settings.test.js
@@ -1,6 +1,6 @@
 const assert = require('assert');
 
-const SettingsBill = require('../settings-bill');
+const SettingsBill = require('../settings-bill.test');
 
 describe('settings-bill', function(){
 
