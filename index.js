1c1b6d95793e3a433445414ff11f5f00b8348b56
bonisiweinnocent <bonisiwe.cukatha@gmail.com>
2021-08-13 15:06:13 +0200

    testing

---
+++ index.js
@@ -0,0 +1,92 @@
+const express = require('express');
+const exphbs  = require('express-handlebars');
+const bodyParser =require('body-parser')
+const SettingsBill = require('./settings-bill');
+const app = express();
+const settingsBill = SettingsBill();
+const moment =require('moment')
+moment().format();
+
+
+
+
+app.use(express.static('public'));
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
+app.engine('handlebars', exphbs({defaultLayout: 'main',layoutsDir: __dirname+'/views/layouts'}));
+app.set('view engine', 'handlebars');
+
+app.get('/', function(req, res){
+    let addClass = "";
+    if(settingsBill.hasReachedWarningLevel()){
+        addClass = "warning"
+    }
+    if(settingsBill.hasReachedCriticalLevel()){
+        addClass = "danger"
+    }
+    if(settingsBill.totals().grandTotal < settingsBill.getSettings().criticalLevel){
+        
+    }
+    res.render('index',{
+        settings: settingsBill.getSettings(),
+       totals: settingsBill.totals(),
+       addClass: addClass
+    });
+});
+
+
+app.post('/settings', function(req, res){
+console.log(req.body);
+
+settingsBill.setSettings({
+    callCost: req.body.callCost,
+    smsCost: req.body.smsCost,
+    warningLevel: req.body.warningLevel,
+    criticalLevel: req.body.criticalLevel
+});
+// console.log(settingsBill.getSettings());
+
+res.redirect('/');
+
+});
+
+app.post('/action' ,function(req, res){
+ 
+
+    console.log(req.body.actionType);
+
+    settingsBill.recordAction(req.body.actionType)
+
+
+
+
+    res.redirect('/');
+
+});
+app.get('/actions', function(req, res){
+    res.render('actions', {actions:settingsBill.actions()});
+     
+
+});
+app.get('/actions/:type', function(req, res){
+    const actions =settingsBill.actions()
+    actions.forEach(elem => {
+        elem.timestamp = moment(elem.timestamp).fromNow()})
+        const type= req.params.type
+   
+    res.render('actions', {actions:settingsBill.actionsFor(type)});
+
+
+})
+    
+
+
+
+const PORT = process.env.PORT || 3011;
+
+app.listen(PORT, function(){
+    console.log("app started ")
+});
